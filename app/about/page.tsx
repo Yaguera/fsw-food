0e8578144c482fbfc48124233fde3cd940af0f53
Yaguera <ya-gotre@hotmail.com>
2024-05-02 16:47:35 -0300

    chore: add husky and lint staged setup

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client" //para adicionar interatividade é necessario o use client (useState é uma interatividade)
-
-const About  = () => {
-    return (
-    <h1>About Page</h1>  
-    );
-}
- 
-export default About ;
