19bb78bcb03ec48828e41953a604f638283222cc
Yaguera <ya-gotre@hotmail.com>
2024-05-06 19:31:53 -0300

    feat: add promo banner to homepage

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -6,11 +6,11 @@ const CategoryList = async () => {
   // renderizar um icone para cada categoria
   const categories = await db.category.findMany({});
   return (
-    <>
+    <div className="grid grid-cols-2 gap-3">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
-    </>
+    </div>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -9,7 +10,20 @@ const Home = () => {
       <div className="px-5 pt-6">
         <Search />
       </div>
-      <CategoryList />
+      <div className="px-3 pt-4">
+        <CategoryList />
+      </div>
+      <div className="pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizza"
+          width={0}
+          height={0}
+          sizes="100vh"
+          quality={100}
+          className="h-auto w-full object-contain"
+        />
+      </div>
     </>
   );
 };
