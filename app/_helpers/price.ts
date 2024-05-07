73c78e9baed3a23e5dbaae0123f5406c8828c305
Yaguera <ya-gotre@hotmail.com>
2024-05-06 22:57:42 -0300

    feat: add product list and product item components

---
+++ app/_helpers/price.ts
@@ -0,0 +1,17 @@
+import { Product } from "@prisma/client";
+
+export const calculateProductTotalPrice = (product: Product) => {
+  if (product.discountPercentage === 0) {
+    return product.price;
+  }
+
+  const discount = product.price * (product.discountPercentage / 100);
+  return product.price - discount;
+};
+
+export const formatCurrency = (value: number): string => {
+  return `R$${Intl.NumberFormat("pt-BR", {
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  }).format(value)}`;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
 
 const Home = () => {
   return (
@@ -23,6 +24,10 @@ const Home = () => {
           quality={100}
           className="h-auto w-full object-contain"
         />
+      </div>
+
+      <div className="pt-6">
+        <ProductList />
       </div>
     </>
   );
